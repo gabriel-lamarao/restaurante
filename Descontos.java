2ccbec1469a0fdc17920f94035d0e79a91601973
ailatias <54558491+ailatias@users.noreply.github.com>
2021-03-12 16:47:29 -0300

    Validador de CPF
    método de validar CPF implementado
    o cadastro do cliente só vai seguir se o CPF for válido

---
+++ Descontos.java
@@ -0,0 +1,33 @@
+package restaurante;
+import java.util.Scanner;
+/**
+ *
+ * @authors Aíla Maciel, Gabriel Lamarão
+ */
+
+public class Descontos {
+	
+	Scanner entrada = new Scanner(System.in);
+
+	protected double desconto;
+
+	public double descontoPadrao(double valor) {
+		desconto = valor * (5 / 100);
+		valor = valor - desconto;
+		return valor;
+	}
+
+	
+	public double descontoManual(double valor) {
+		System.out.println("Insira a porcentagem do desconto: ");
+		desconto = entrada.nextDouble();
+		desconto = valor * (desconto / 100);
+		valor = valor - desconto;
+		return valor;
+	}
+	
+	public double descontoFidelidade(double valor, Cliente cliente) {
+		return desconto;
+	}
+	// Desconto de aniversário aniversario(Data aniversario) 30% desconto
+}
